4bfd3e4b7657a94deef98b02f6ee1f1e255decca
WU MUXIAN <wumuxian1988@gmail.com>
2018-03-06 15:23:15 +0800

    added concurrent upload and downloader for S3

---
+++ aws/concurrent_downloader.go
@@ -0,0 +1,79 @@
+package aws
+
+// ConcurrentDownloader is used to download multiple files to S3 concurrently.
+type ConcurrentDownloader struct {
+	DownloadContent map[string][]byte
+
+	bucketName string
+	region     string
+
+	downloadChan     chan map[string][]byte
+	DownloadComplete chan bool
+}
+
+// NewConcurrentDownloader creates a new downloader
+func NewConcurrentDownloader(bucketName, region string) (downloader *ConcurrentDownloader) {
+	downloader = new(ConcurrentDownloader)
+
+	downloader.DownloadContent = make(map[string][]byte)
+	downloader.bucketName = bucketName
+
+	downloader.downloadChan = make(chan map[string][]byte)
+	downloader.DownloadComplete = make(chan bool)
+
+	return
+}
+
+// Download marks to be downloaded path, it will not execute the actual downloading.
+func (downloader *ConcurrentDownloader) Download(path string) {
+	downloader.DownloadContent[path] = nil
+}
+
+// Execute executes the downloading process.
+func (downloader *ConcurrentDownloader) Execute() {
+	for path := range downloader.DownloadContent {
+		go func(path string) {
+			content, err := GetS3Service(downloader.region).ReadFromS3Concurrently(downloader.bucketName, path)
+			if err == nil {
+				downloader.downloadChan <- map[string][]byte{
+					path: content,
+				}
+			} else {
+				downloader.downloadChan <- map[string][]byte{
+					path: []byte("error"),
+				}
+			}
+
+		}(path)
+	}
+
+	downloader.checkProgress()
+}
+
+func (downloader *ConcurrentDownloader) checkProgress() {
+	go func() {
+		for {
+			for path, content := range <-downloader.downloadChan {
+				downloader.DownloadContent[path] = content
+			}
+
+			completed := true
+			err := false
+
+			for _, content := range downloader.DownloadContent {
+				if content == nil {
+					completed = false
+					break
+				} else if string(content) == "error" {
+					err = true
+					break
+				}
+			}
+
+			if completed {
+				downloader.DownloadComplete <- !err
+				break
+			}
+		}
+	}()
+}
---
+++ aws/concurrent_uploader.go
@@ -0,0 +1,79 @@
+package aws
+
+// ConcurrentUploader is used to upload multiple files to S3 concurrently.
+type ConcurrentUploader struct {
+	uploadStatus  map[string]int // status 0 = pending, 1 = completed, -1 = failed
+	uploadContent map[string][]byte
+
+	region     string
+	bucketName string
+
+	uploadChan     chan map[string]error
+	UploadComplete chan bool
+}
+
+// NewConcurrentUploader creates a new uploader
+func NewConcurrentUploader(bucketName, region string) (uploader *ConcurrentUploader) {
+	uploader = new(ConcurrentUploader)
+
+	uploader.uploadStatus = make(map[string]int)
+	uploader.uploadContent = make(map[string][]byte)
+	uploader.bucketName = bucketName
+
+	uploader.uploadChan = make(chan map[string]error)
+	uploader.UploadComplete = make(chan bool)
+
+	return
+}
+
+// Upload marks the content with the upload path, it will not execute the actual uploading.
+func (uploader *ConcurrentUploader) Upload(content []byte, path string) {
+	uploader.uploadContent[path] = content
+	uploader.uploadStatus[path] = 0
+}
+
+// Execute executes the uploading process.
+func (uploader *ConcurrentUploader) Execute() {
+	for path, content := range uploader.uploadContent {
+		go func(path string, content []byte) {
+			err := GetS3Service(uploader.region).UploadToS3Concurrently(content, uploader.bucketName, path)
+			uploader.uploadChan <- map[string]error{
+				path: err,
+			}
+		}(path, content)
+	}
+
+	uploader.checkProgress()
+}
+
+func (uploader *ConcurrentUploader) checkProgress() {
+	go func() {
+		for {
+			for path, err := range <-uploader.uploadChan {
+				if err == nil {
+					uploader.uploadStatus[path] = 1
+				} else {
+					uploader.uploadStatus[path] = -1
+				}
+			}
+
+			completed := true
+			err := false
+
+			for _, status := range uploader.uploadStatus {
+				if status == 0 {
+					completed = false
+					break
+				} else if status == -1 {
+					err = true
+					break
+				}
+			}
+
+			if completed {
+				uploader.UploadComplete <- !err
+				break
+			}
+		}
+	}()
+}
