7e75bae223728d7c08d7afba180a8c7478d771fe
WU MUXIAN <wumuxian1988@gmail.com>
2018-10-12 17:35:45 +0800

    return the search success status as well

--- datastructure/shared/utils.go
+++ datastructure/shared/utils.go
@@ -1,18 +1,18 @@
 package shared
 
 // BinarySearch performs binary search for a value in sorted value slice.
-func BinarySearch(sortedValues []interface{}, searchValue interface{}, comparator Comparator) int {
+func BinarySearch(sortedValues []interface{}, searchValue interface{}, comparator Comparator) (int, bool) {
 	start := 0
 	end := len(sortedValues) - 1
 	for start <= end {
 		mid := (start + end) / 2
 		if comparator(sortedValues[mid], searchValue) == 0 {
-			return mid
+			return mid, true
 		} else if comparator(sortedValues[mid], searchValue) > 0 {
 			end = mid - 1
 		} else {
 			start = mid + 1
 		}
 	}
-	return end
+	return end, false
 }
--- datastructure/shared/utils_test.go
+++ datastructure/shared/utils_test.go
@@ -9,12 +9,28 @@ import (
 func TestBinarySearch(t *testing.T) {
 	sortedValues := []interface{}{1, 3, 5, 7, 9, 11, 13, 15, 17, 19}
 	Convey("Inorder Traverse Should Generate Sorted Values", t, func() {
-		So(BinarySearch(sortedValues, 3, IntComparator), ShouldEqual, 1)
-		So(BinarySearch(sortedValues, 9, IntComparator), ShouldEqual, 4)
-		So(BinarySearch(sortedValues, 5, IntComparator), ShouldEqual, 2)
-		So(BinarySearch(sortedValues, 6, IntComparator), ShouldEqual, 2)
-		So(BinarySearch(sortedValues, 16, IntComparator), ShouldEqual, 7)
-		So(BinarySearch(sortedValues, 0, IntComparator), ShouldEqual, -1)
-		So(BinarySearch(sortedValues, 20, IntComparator), ShouldEqual, 9)
+		var index int
+		var found bool
+		index, found = BinarySearch(sortedValues, 3, IntComparator)
+		So(index, ShouldEqual, 1)
+		So(found, ShouldEqual, true)
+		index, found = BinarySearch(sortedValues, 9, IntComparator)
+		So(index, ShouldEqual, 4)
+		So(found, ShouldEqual, true)
+		index, found = BinarySearch(sortedValues, 5, IntComparator)
+		So(index, ShouldEqual, 2)
+		So(found, ShouldEqual, true)
+		index, found = BinarySearch(sortedValues, 6, IntComparator)
+		So(index, ShouldEqual, 2)
+		So(found, ShouldEqual, false)
+		index, found = BinarySearch(sortedValues, 16, IntComparator)
+		So(index, ShouldEqual, 7)
+		So(found, ShouldEqual, false)
+		index, found = BinarySearch(sortedValues, 0, IntComparator)
+		So(index, ShouldEqual, -1)
+		So(found, ShouldEqual, false)
+		index, found = BinarySearch(sortedValues, 20, IntComparator)
+		So(index, ShouldEqual, 9)
+		So(found, ShouldEqual, false)
 	})
 }
