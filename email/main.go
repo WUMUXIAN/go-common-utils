060accd1a4b7fe53469c65a6e02179fe4f381d55
WU MUXIAN <wumuxian1988@gmail.com>
2018-11-12 17:34:09 +0800

    only check for host, don't try to send actual message to the email address in reachability test.

--- email/main.go
+++ email/main.go
@@ -40,35 +40,35 @@ func ValidateReachability(email string, timeout time.Duration) error {
 			return
 		}
 		fmt.Println("host", mx[0].Host, "reachable")
-		client, err := smtp.Dial(fmt.Sprintf("%s:%d", mx[0].Host, 25))
-		if err != nil {
-			fmt.Println("host", mx[0].Host, "dial failed")
-			c <- err
-			return
-		}
-		fmt.Println("host", mx[0].Host, "dial done")
-		defer client.Close()
-		err = client.Hello("checkmail.me")
-		if err != nil {
-			c <- err
-			fmt.Println("host", mx[0].Host, "hello not sent")
-			return
-		}
-		fmt.Println("host", mx[0].Host, "hello sent")
-		err = client.Mail("wumuxian1988@gmail.com")
-		if err != nil {
-			c <- err
-			fmt.Println("host", mx[0].Host, "mail command not successful")
-			return
-		}
-		fmt.Println("host", mx[0].Host, "mail command successful")
-		err = client.Rcpt(email)
-		if err != nil {
-			c <- err
-			fmt.Println("host", mx[0].Host, "recpt not done")
-			return
-		}
-		fmt.Println("host", mx[0].Host, "recpt done")
+		// client, err := smtp.Dial(fmt.Sprintf("%s:%d", mx[0].Host, 25))
+		// if err != nil {
+		// 	fmt.Println("host", mx[0].Host, "dial failed")
+		// 	c <- err
+		// 	return
+		// }
+		// fmt.Println("host", mx[0].Host, "dial done")
+		// defer client.Close()
+		// err = client.Hello("checkmail.me")
+		// if err != nil {
+		// 	c <- err
+		// 	fmt.Println("host", mx[0].Host, "hello not sent")
+		// 	return
+		// }
+		// fmt.Println("host", mx[0].Host, "hello sent")
+		// err = client.Mail("wumuxian1988@gmail.com")
+		// if err != nil {
+		// 	c <- err
+		// 	fmt.Println("host", mx[0].Host, "mail command not successful")
+		// 	return
+		// }
+		// fmt.Println("host", mx[0].Host, "mail command successful")
+		// err = client.Rcpt(email)
+		// if err != nil {
+		// 	c <- err
+		// 	fmt.Println("host", mx[0].Host, "recpt not done")
+		// 	return
+		// }
+		// fmt.Println("host", mx[0].Host, "recpt done")
 		c <- nil
 	}()
 
