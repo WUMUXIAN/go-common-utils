82897860f37ff7091cf613e67dee5035bc1a8f66
Shebin <sv@tectusdreamlab.com>
2019-03-11 15:15:49 +0800

    added uint64 support

---
+++ slice/uint64.go
@@ -0,0 +1,157 @@
+package slice
+
+import (
+	"fmt"
+	"math/rand"
+)
+
+// IndexOfUInt64 gets the index of an uint64 element in an uint64 slice
+func IndexOfUInt64(x []uint64, y uint64) int {
+	for i, v := range x {
+		if v == y {
+			return i
+		}
+	}
+	return -1
+}
+
+// ContainsUInt64 checks whether an uint64 element is in an uint64 slice
+func ContainsUInt64(x []uint64, y uint64) bool {
+	return IndexOfUInt64(x, y) != -1
+}
+
+// EqualsUInt64s checks whether two uint64 slice has the same elements
+func EqualsUInt64s(x []uint64, y []uint64) bool {
+	if len(x) != len(y) {
+		return false
+	}
+
+	for i := 0; i < len(x); i++ {
+		if x[i] != y[i] {
+			return false
+		}
+	}
+
+	return true
+}
+
+// CopyUInt64s makes a new uint64 slice that copies the content of the given uint64 slice
+func CopyUInt64s(x []uint64) []uint64 {
+	return append([]uint64{}, x...)
+}
+
+// CutUInt64s cuts an uint64 slice by removing the elements starts from i and ends at j-1
+func CutUInt64s(x []uint64, i, j int) ([]uint64, error) {
+	if i < 0 || j > len(x) {
+		return x, fmt.Errorf("out of bound")
+	}
+	if i >= j {
+		return x, fmt.Errorf("%d must be smaller than %d", i, j)
+	}
+	return append(x[:i], x[j:]...), nil
+}
+
+// RemoveUInt64 removes an uint64 from a given uint64 slice by value
+func RemoveUInt64(x []uint64, y uint64) []uint64 {
+	index := IndexOfUInt64(x, y)
+	if index != -1 {
+		return append(x[:index], x[(index+1):]...)
+	}
+	return x
+}
+
+// RemoveUInt64At removes an uint64 from a given uint64 slice by index
+func RemoveUInt64At(x []uint64, index int) ([]uint64, error) {
+	if index < 0 || index > len(x) {
+		return x, fmt.Errorf("out of bound")
+	}
+	return append(x[:index], x[(index+1):]...), nil
+}
+
+// InsertUInt64At inserts an uint64 value into a given uint64 slice at given index
+func InsertUInt64At(x []uint64, y uint64, index int) ([]uint64, error) {
+	if index < 0 || index > len(x) {
+		return x, fmt.Errorf("out of bound")
+	}
+	x = append(x, 0)
+	copy(x[index+1:], x[index:])
+	x[index] = y
+	return x, nil
+}
+
+// InsertUInt64sAt inserts a uint64 slice into a given uint64 slice at given index
+func InsertUInt64sAt(x []uint64, y []uint64, index int) ([]uint64, error) {
+	if index < 0 || index > len(x) {
+		return x, fmt.Errorf("out of bound")
+	}
+	return append(x[:index], append(y, x[index:]...)...), nil
+}
+
+// PopFirstUInt64 pops the first value of an uint64 slice
+func PopFirstUInt64(x []uint64) (uint64, []uint64, error) {
+	if len(x) == 0 {
+		return 0, nil, fmt.Errorf("no value to pop")
+	}
+	return x[0], x[1:], nil
+}
+
+// PopLastUInt64 pops the last value of an uint64 slice
+func PopLastUInt64(x []uint64) (uint64, []uint64, error) {
+	if len(x) == 0 {
+		return 0, nil, fmt.Errorf("no value to pop")
+	}
+	return x[len(x)-1], x[:len(x)-1], nil
+}
+
+// FilterUInt64s filters an uint64 slice by the given filter function
+func FilterUInt64s(x []uint64, filter func(uint64) bool) []uint64 {
+	y := x[:0]
+	for _, v := range x {
+		if filter(v) {
+			y = append(y, v)
+		}
+	}
+	return y
+}
+
+// ReverseUInt64s reverses an uint64 slice
+func ReverseUInt64s(x []uint64) []uint64 {
+	for i := len(x)/2 - 1; i >= 0; i-- {
+		opp := len(x) - 1 - i
+		x[i], x[opp] = x[opp], x[i]
+	}
+	return x
+}
+
+// ShuffleUInt64s shuffles an uint64 slice
+func ShuffleUInt64s(x []uint64) []uint64 {
+	for i := len(x) - 1; i > 0; i-- {
+		j := rand.Intn(i + 1)
+		x[i], x[j] = x[j], x[i]
+	}
+	return x
+}
+
+// MergeUInt64s merges two uint64 slice with specific excluded values
+func MergeUInt64s(x []uint64, y []uint64, excludes ...uint64) []uint64 {
+	traceMap := make(map[uint64]bool)
+	result := make([]uint64, 0)
+	for _, ex := range excludes {
+		traceMap[ex] = true
+	}
+	// We preserve the order by x and then y
+	for _, v := range x {
+		if !traceMap[v] {
+			traceMap[v] = true
+			result = append(result, v)
+		}
+	}
+
+	for _, v := range y {
+		if !traceMap[v] {
+			traceMap[v] = true
+			result = append(result, v)
+		}
+	}
+	return result
+}
---
+++ slice/uint64_test.go
@@ -0,0 +1,155 @@
+package slice
+
+import (
+	"testing"
+
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+func TestUInt64(t *testing.T) {
+	var index int
+	var boolean bool
+	x := []uint64{1, 2, 3, 4, 5}
+	y := []uint64{6, 7, 8, 9, 10}
+	z := []uint64{11, 12, 13, 14, 15, 16}
+
+	Convey("Test The UInt64 Slice Functions\n", t, func() {
+		Convey("IndexOfUInt64", func() {
+			index = IndexOfUInt64(x, 1)
+			So(index, ShouldEqual, 0)
+			index = IndexOfUInt64(x, 6)
+			So(index, ShouldEqual, -1)
+		})
+
+		Convey("ContainsUInt64", func() {
+			boolean = ContainsUInt64(x, 1)
+			So(boolean, ShouldBeTrue)
+			boolean = ContainsUInt64(x, 6)
+			So(boolean, ShouldBeFalse)
+		})
+
+		Convey("EqualsUInt64s", func() {
+			boolean = EqualsUInt64s(x, x)
+			So(boolean, ShouldBeTrue)
+			boolean = EqualsUInt64s(x, y)
+			So(boolean, ShouldBeFalse)
+			boolean = EqualsUInt64s(x, z)
+			So(boolean, ShouldBeFalse)
+			boolean = EqualsUInt64s(y, z)
+			So(boolean, ShouldBeFalse)
+		})
+
+		Convey("CopyUInt64s", func() {
+			x1 := CopyUInt64s(x)
+			boolean = EqualsUInt64s(x, x1)
+			So(boolean, ShouldBeTrue)
+		})
+
+		Convey("CutUInt64s", func() {
+			x1, err := CutUInt64s(x, 2, 4)
+			So(err, ShouldBeNil)
+			So(x1, ShouldResemble, []uint64{1, 2, 5})
+			// Since it's a in-place operation, x will be changed, we need to re-init it.
+			x = []uint64{1, 2, 3, 4, 5}
+			_, err = CutUInt64s(x, -1, 6)
+			So(err, ShouldNotBeNil)
+			_, err = CutUInt64s(x, 5, 4)
+			So(err, ShouldNotBeNil)
+		})
+
+		Convey("RemoveUInt64", func() {
+			x1 := RemoveUInt64(x, 3)
+			So(x1, ShouldResemble, []uint64{1, 2, 4, 5})
+			x2 := RemoveUInt64(x1, 10)
+			So(x2, ShouldResemble, []uint64{1, 2, 4, 5})
+			// Since it's a in-place operation, x will be changed, we need to re-init it.
+			x = []uint64{1, 2, 3, 4, 5}
+		})
+
+		Convey("RemoveUInt64At", func() {
+			x1, err := RemoveUInt64At(x, 3)
+			So(err, ShouldBeNil)
+			So(x1, ShouldResemble, []uint64{1, 2, 3, 5})
+			_, err = RemoveUInt64At(x, 8)
+			So(err, ShouldNotBeNil)
+			// Since it's a in-place operation, x will be changed, we need to re-init it.
+			x = []uint64{1, 2, 3, 4, 5}
+		})
+
+		Convey("InsertUInt64At", func() {
+			x1, err := InsertUInt64At(x, 6, 3)
+			So(err, ShouldBeNil)
+			So(x1, ShouldResemble, []uint64{1, 2, 3, 6, 4, 5})
+			_, err = InsertUInt64At(x, 6, 8)
+			So(err, ShouldNotBeNil)
+			// Since it's a in-place operation, x will be changed, we need to re-init it.
+			x = []uint64{1, 2, 3, 4, 5}
+		})
+
+		Convey("InsertUInt64sAt", func() {
+			x1, err := InsertUInt64sAt(x, y, 3)
+			So(err, ShouldBeNil)
+			So(x1, ShouldResemble, []uint64{1, 2, 3, 6, 7, 8, 9, 10, 4, 5})
+			_, err = InsertUInt64sAt(x, y, 8)
+			So(err, ShouldNotBeNil)
+			// Since it's a in-place operation, x will be changed, we need to re-init it.
+			x = []uint64{1, 2, 3, 4, 5}
+		})
+
+		Convey("PopFirstUInt64", func() {
+			v, x1, err := PopFirstUInt64(x)
+			So(err, ShouldBeNil)
+			So(x1, ShouldResemble, []uint64{2, 3, 4, 5})
+			So(v, ShouldResemble, uint64(1))
+			_, _, err = PopFirstUInt64([]uint64{})
+			So(err, ShouldNotBeNil)
+			// Since it's a in-place operation, x will be changed, we need to re-init it.
+			x = []uint64{1, 2, 3, 4, 5}
+		})
+
+		Convey("PopLastUInt64", func() {
+			v, x1, err := PopLastUInt64(x)
+			So(err, ShouldBeNil)
+			So(x1, ShouldResemble, []uint64{1, 2, 3, 4})
+			So(v, ShouldResemble, uint64(5))
+			_, _, err = PopLastUInt64([]uint64{})
+			So(err, ShouldNotBeNil)
+			// Since it's a in-place operation, x will be changed, we need to re-init it.
+			x = []uint64{1, 2, 3, 4, 5}
+		})
+
+		Convey("FilterUInt64s", func() {
+			x1 := FilterUInt64s(x, func(v uint64) bool {
+				return v == 2 || v == 3
+			})
+			So(x1, ShouldResemble, []uint64{2, 3})
+			x1 = FilterUInt64s(x, func(v uint64) bool {
+				return v == 10
+			})
+			So(x1, ShouldResemble, []uint64{})
+			// Since it's a in-place operation, x will be changed, we need to re-init it.
+			x = []uint64{1, 2, 3, 4, 5}
+		})
+
+		Convey("ReverseUInt64s", func() {
+			x1 := ReverseUInt64s(x)
+			So(x1, ShouldResemble, []uint64{5, 4, 3, 2, 1})
+			// Since it's a in-place operation, x will be changed, we need to re-init it.
+			x = []uint64{1, 2, 3, 4, 5}
+		})
+
+		Convey("ShuffleUInt64s", func() {
+			x1 := ShuffleUInt64s(x)
+			So(x1, ShouldNotResemble, []uint64{1, 2, 3, 4, 5})
+		})
+
+		Convey("MergeUInt64s", func() {
+			x1 := []uint64{1, 2, 3}
+			x2 := []uint64{2, 3, 4, 5}
+			merged := MergeUInt64s(x1, x2)
+			So(merged, ShouldResemble, []uint64{1, 2, 3, 4, 5})
+			merged = MergeUInt64s(x1, x2, 1, 2, 5)
+			So(merged, ShouldResemble, []uint64{3, 4})
+		})
+	})
+}
