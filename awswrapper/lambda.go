86a59491d438aa092ff0851e311a1b5e1e265a74
Shebin <sv@tectusdreamlab.com>
2020-07-23 11:50:36 +0800

    added custom http client for the lambda service

--- awswrapper/lambda.go
+++ awswrapper/lambda.go
@@ -1,14 +1,30 @@
 package awswrapper
 
 import (
+	"fmt"
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/lambda"
+	"golang.org/x/net/http2"
+	"net"
+	"net/http"
+	"time"
 )
 
 // S3Service represents a S3 service.
 type LambdaService struct {
 	region  string
 	service *lambda.Lambda
+}
+
+type HTTPClientSettings struct {
+	Connect          time.Duration
+	ConnKeepAlive    time.Duration
+	ExpectContinue   time.Duration
+	IdleConn         time.Duration
+	MaxAllIdleConns  int
+	MaxHostIdleConns int
+	ResponseHeader   time.Duration
+	TLSHandshake     time.Duration
 }
 
 var (
@@ -20,8 +36,26 @@ func GetLambdaService(region string) *LambdaService {
 	if lambdaService, ok := lambdaServices[region]; ok {
 		return lambdaService
 	}
+
+	httpClient, err := NewHTTPClientWithSettings(HTTPClientSettings{
+		Connect:          2 * time.Minute,
+		ExpectContinue:   1 * time.Second,
+		IdleConn:         90 * time.Second,
+		ConnKeepAlive:    30 * time.Second,
+		MaxAllIdleConns:  100,
+		MaxHostIdleConns: 10,
+		ResponseHeader:   10 * time.Second,
+		TLSHandshake:     10 * time.Second,
+	})
+	if err != nil {
+		fmt.Println("Got an error creating custom HTTP client:")
+		fmt.Println(err)
+		return nil
+	}
+
 	svc := lambda.New(sess, &aws.Config{
-		Region: aws.String(region),
+		Region:     aws.String(region),
+		HTTPClient: httpClient,
 	})
 	lambdaService := &LambdaService{
 		region:  region,
@@ -38,4 +72,32 @@ func (o *LambdaService) Invoke(functionName string, payload []byte, isEvent bool
 	}
 
 	return o.service.Invoke(&lambda.InvokeInput{FunctionName: aws.String(functionName), InvocationType: aws.String(invocationType), Payload: payload})
+}
+
+func NewHTTPClientWithSettings(httpSettings HTTPClientSettings) (*http.Client, error) {
+	var client http.Client
+	tr := &http.Transport{
+		ResponseHeaderTimeout: httpSettings.ResponseHeader,
+		Proxy:                 http.ProxyFromEnvironment,
+		DialContext: (&net.Dialer{
+			KeepAlive: httpSettings.ConnKeepAlive,
+			DualStack: true,
+			Timeout:   httpSettings.Connect,
+		}).DialContext,
+		MaxIdleConns:          httpSettings.MaxAllIdleConns,
+		IdleConnTimeout:       httpSettings.IdleConn,
+		TLSHandshakeTimeout:   httpSettings.TLSHandshake,
+		MaxIdleConnsPerHost:   httpSettings.MaxHostIdleConns,
+		ExpectContinueTimeout: httpSettings.ExpectContinue,
+	}
+
+	// So client makes HTTP/2 requests
+	err := http2.ConfigureTransport(tr)
+	if err != nil {
+		return &client, err
+	}
+
+	return &http.Client{
+		Transport: tr,
+	}, nil
 }
